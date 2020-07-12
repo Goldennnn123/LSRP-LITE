b9bad8d3d43a26dcfd07d8e8ae014c55a786653c
Asan Rodnuan <AsanRodnuan2546@gmail.com>
2020-07-12 11:56:04 -0700

    เพื่มฐ้านข้อมูล Ajail kick ban

--- database/samp.sql
+++ database/samp.sql
@@ -1,14 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2020 at 09:28 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.27
+-- Generation Time: Jul 12, 2020 at 08:44 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+
+--สร้างใหม่--
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +21,142 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `samp`
+-- Database: `database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ajail_logs`
+--
+
+CREATE TABLE `ajail_logs` (
+  `id` int(11) NOT NULL,
+  `JailedDBID` int(11) NOT NULL,
+  `JailedName` varchar(32) NOT NULL,
+  `Reason` varchar(128) NOT NULL,
+  `Date` varchar(90) NOT NULL,
+  `JailedBy` varchar(32) NOT NULL,
+  `Time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ajail_logs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bannedlist`
+--
+
+CREATE TABLE `bannedlist` (
+  `id` int(11) NOT NULL,
+  `CharacterDBID` int(11) NOT NULL,
+  `MasterDBID` int(11) NOT NULL,
+  `CharacterName` varchar(32) NOT NULL,
+  `Reason` varchar(128) NOT NULL,
+  `Date` varchar(90) NOT NULL,
+  `BannedBy` varchar(32) NOT NULL,
+  `IpAddress` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ban_logs`
+--
+
+CREATE TABLE `ban_logs` (
+  `id` int(11) NOT NULL,
+  `CharacterDBID` int(11) NOT NULL,
+  `MasterDBID` int(11) NOT NULL,
+  `CharacterName` varchar(32) NOT NULL,
+  `Reason` varchar(128) NOT NULL,
+  `BannedBy` varchar(32) NOT NULL,
+  `Date` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters`
+--
+
+CREATE TABLE `characters` (
+  `char_dbid` int(11) NOT NULL,
+  `master_id` int(11) NOT NULL,
+  `char_name` varchar(255) NOT NULL,
+  `pTutorial` tinyint(1) NOT NULL DEFAULT 0,
+  `pAdmin` int(11) NOT NULL,
+  `pLevel` int(11) NOT NULL DEFAULT 0,
+  `pExp` int(11) NOT NULL DEFAULT 0,
+  `pLastSkin` int(11) NOT NULL DEFAULT 264,
+  `pFaction` int(11) DEFAULT 0,
+  `pCash` int(11) NOT NULL DEFAULT 0,
+  `pSpawnPoint` int(11) NOT NULL DEFAULT 0,
+  `pSpawnHouse` int(11) NOT NULL DEFAULT 0,
+  `pTimeout` int(11) NOT NULL DEFAULT 0,
+  `pHealth` float NOT NULL DEFAULT 100,
+  `pArmour` float NOT NULL DEFAULT 0,
+  `pLastPosX` float NOT NULL DEFAULT 0,
+  `pLastPosY` float NOT NULL DEFAULT 0,
+  `pLastPosZ` float NOT NULL DEFAULT 0,
+  `pLastInterior` int(11) NOT NULL DEFAULT 0,
+  `pLastWorld` int(11) NOT NULL DEFAULT 0,
+  `pJob` int(11) NOT NULL DEFAULT 0,
+  `pSideJob` int(11) NOT NULL DEFAULT 0,
+  `pCareer` int(11) NOT NULL DEFAULT 0,
+  `pPaycheck` int(11) NOT NULL DEFAULT 0,
+  `pFishes` int(11) NOT NULL DEFAULT 0,
+  `pAdminjailed` int(11) NOT NULL,
+  `pAdminjailTime` int(11) NOT NULL,
+  `pWeapon0` int(11) NOT NULL,
+  `pWeapon1` int(11) NOT NULL,
+  `pWeapon2` int(11) NOT NULL,
+  `pWeapon3` int(11) NOT NULL,
+  `pWeapon4` int(11) NOT NULL,
+  `pWeapon5` int(11) NOT NULL,
+  `pWeapon6` int(11) NOT NULL,
+  `pWeapon7` int(11) NOT NULL,
+  `pWeapon8` int(11) NOT NULL,
+  `pWeapon9` int(11) NOT NULL,
+  `pWeapon10` int(11) NOT NULL,
+  `pWeapon11` int(11) NOT NULL,
+  `pWeapon12` int(11) NOT NULL,
+  `pWeaponsAmmo0` int(11) NOT NULL,
+  `pWeaponsAmmo1` int(11) NOT NULL,
+  `pWeaponsAmmo2` int(11) NOT NULL,
+  `pWeaponsAmmo3` int(11) NOT NULL,
+  `pWeaponsAmmo4` int(11) NOT NULL,
+  `pWeaponsAmmo5` int(11) NOT NULL,
+  `pWeaponsAmmo6` int(11) NOT NULL,
+  `pWeaponsAmmo7` int(11) NOT NULL,
+  `pWeaponsAmmo8` int(11) NOT NULL,
+  `pWeaponsAmmo9` int(11) NOT NULL,
+  `pWeaponsAmmo10` int(11) NOT NULL,
+  `pWeaponsAmmo11` int(11) NOT NULL,
+  `pWeaponsAmmo12` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `characters`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kick_logs`
+--
+
+CREATE TABLE `kick_logs` (
+  `id` int(11) NOT NULL,
+  `KickedDBID` int(11) NOT NULL,
+  `KickedName` varchar(32) NOT NULL,
+  `Reason` varchar(128) NOT NULL,
+  `KickedBy` varchar(32) NOT NULL,
+  `Date` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -36,48 +172,31 @@ CREATE TABLE `masters` (
   `admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `characters`
+-- Dumping data for table `masters`
 --
-
-CREATE TABLE `characters` (
-  `char_dbid` int(11) NOT NULL,
-  `master_id` int(11) NOT NULL,
-  `char_name` varchar(255) NOT NULL,
-  `pTutorial` tinyint(1) NOT NULL DEFAULT '0',
-  `pLevel` int(11) NOT NULL DEFAULT '0',
-  `pExp` int(11) NOT NULL DEFAULT '0',
-  `pLastSkin` int(11) NOT NULL DEFAULT '264',
-  `pFaction` int(11) DEFAULT '0',
-  `pCash` int(11) NOT NULL DEFAULT '0',
-  `pSpawnPoint` int(11) NOT NULL DEFAULT '0',
-  `pSpawnHouse` int(11) NOT NULL DEFAULT '0',
-  `pTimeout` int(11) NOT NULL DEFAULT '0',
-  `pHealth` float NOT NULL DEFAULT '100.0',
-  `pArmour` float NOT NULL DEFAULT '0.0',
-  `pLastPosX` float NOT NULL DEFAULT '0.0',
-  `pLastPosY` float NOT NULL DEFAULT '0.0',
-  `pLastPosZ` float NOT NULL DEFAULT '0.0',
-  `pLastInterior` int(11) NOT NULL DEFAULT '0',
-  `pLastWorld` int(11) NOT NULL DEFAULT '0',
-  `pJob` int(11) NOT NULL DEFAULT '0',
-  `pSideJob` int(11) NOT NULL DEFAULT '0',
-  `pCareer` int(11) NOT NULL DEFAULT '0',
-  `pPaycheck` int(11) NOT NULL DEFAULT '0',
-  `pFishes` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `masters`
+-- Indexes for table `ajail_logs`
 --
-ALTER TABLE `masters`
-  ADD PRIMARY KEY (`acc_dbid`);
+ALTER TABLE `ajail_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bannedlist`
+--
+ALTER TABLE `bannedlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ban_logs`
+--
+ALTER TABLE `ban_logs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `characters`
@@ -86,20 +205,56 @@ ALTER TABLE `characters`
   ADD PRIMARY KEY (`char_dbid`);
 
 --
+-- Indexes for table `kick_logs`
+--
+ALTER TABLE `kick_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `masters`
+--
+ALTER TABLE `masters`
+  ADD PRIMARY KEY (`acc_dbid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `masters`
+-- AUTO_INCREMENT for table `ajail_logs`
 --
-ALTER TABLE `masters`
-  MODIFY `acc_dbid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `ajail_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bannedlist`
+--
+ALTER TABLE `bannedlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ban_logs`
+--
+ALTER TABLE `ban_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `characters`
 --
 ALTER TABLE `characters`
-  MODIFY `char_dbid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `char_dbid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kick_logs`
+--
+ALTER TABLE `kick_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `masters`
+--
+ALTER TABLE `masters`
+  MODIFY `acc_dbid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

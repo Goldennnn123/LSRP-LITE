865c8b5273a0d2fdb314c4f04858b2016b7b190f
Backerss <AsanRodnuan2546@gmail.com>
2021-08-27 12:24:34 +0700

    แก้ตัวจัดการไฟล

---
+++ scriptfiles/db/global.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `global` (
+    `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `G_BITSAMP` int(11) NOT NULL,
+    `G_GovCash` int(11) NOT NULL,
+    `G_BITStock` float(11) NOT NULL,
+        PRIMARY KEY
+            (ID)
+);
